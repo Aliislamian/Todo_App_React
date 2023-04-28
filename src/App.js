84b9f3286a7da8da0dd141314f55220fbf86d765
Aliislamian <aliislamian123@gmail.com>
2023-04-29 00:44:09 +0500

    Todo App Done In react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Box from "./components/Box";
+import Input from "./components/Input";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const removeTodo = (id) => {
+    console.log(id);
+      const newTodos =  todos.filter(
+        (d, index) => {
+          if(index !== id)
+          {
+            return true
+          } else{
+            return false
+          }
+        }
+      )
+      setTodos(newTodos)
+  }
+  const addTodoHandler = (item) => {
+      // console.log(item);
+      setTodos(
+        [
+          ...todos,
+          {
+            item,
+            time: new Date().toLocaleTimeString()
+          }
+        ]
+      )
+  }
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black h-screen p-3">
+      <div className=" rounded mx-auto max-w-[750px] min-h-[550] shadow-2xl bg-white">
+      <Input handler={addTodoHandler} />
+      <Box data={todos} removeHandler={removeTodo}/>
+      </div>
     </div>
   );
 }
